8fc8dfb368153de3ab5592b846f5415ade87f062
Jared Cochran <j1cochra@odu.edu>
2026-01-16 15:32:39 -0500

    cmake is wild

--- src/radio/Radio.cpp
+++ src/radio/Radio.cpp
@@ -2,17 +2,15 @@
 
 namespace csics::radio {
 
-RadioDeviceArgs::RadioDeviceArgs(): device_type(DeviceType::DEFAULT) {
-}
+RadioDeviceArgs::RadioDeviceArgs()
+    : device_type(DeviceType::DEFAULT), args(std::monostate{}) {}
 
 #ifdef CSICS_USE_UHD
-    UsrpArgs::operator RadioDeviceArgs() const {
-        RadioDeviceArgs args;
-        args.device_type = DeviceType::USRP;
-        args.args = *this;
-        return args;
-    }
-
+UsrpArgs::operator RadioDeviceArgs() const {
+    RadioDeviceArgs args;
+    args.device_type = DeviceType::USRP;
+    args.args = *this;
+    return args;
+}
 #endif
-
-};
+}  // namespace csics::radio
